bc1e1b6cf74a0f87823665c16bc40b1eed7e5a62
jhet <jhetdizon41@gmail.com>
2024-07-10 20:57:26 +0800

    created the admin model

---
+++ database/migrations/2024_07_10_125546_create_admin_table.php
@@ -0,0 +1,34 @@
+<?php
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAdminTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('admin', function (Blueprint $table) {
+            $table->id('adminId'); // Primary key
+            $table->string('adminName');
+            $table->string('adminEmail')->unique();
+            $table->string('adminPassword');
+            $table->timestamps(); // Adds created_at and updated_at columns
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('admins');
+    }
+}
+
