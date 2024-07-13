8eeed95cc6375f88f6efa66d6e531c00cf734568
jia <obillojoewen@gmail.com>
2024-07-13 18:34:54 +0800

    added meal listing page v.1

--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,7 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
+    Route::get('/menu-listing', function () {
+        return view('menu-listing');
+    })->name('menu-listing');
 });
