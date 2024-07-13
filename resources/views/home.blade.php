5b63df74443ec219b43bcd0b14dc3c34cd692b35
bbJerr <jerrakirstenr22@gmail.com>
2024-07-13 16:08:13 +0800

    commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -33,25 +33,25 @@
             <!-- Full-width images with number and caption text -->
             <div class="mySlides fade">
                 <div class="numbertext">1 / 4</div>
-                <img src="{{ asset('images/img2.jpg') }}" style="width:100%">
+                <img src="{{ asset('images/img2.jpg') }}">
                 <div class="text">Caption Text</div>
             </div>
         
             <div class="mySlides fade">
                 <div class="numbertext">2 / 4</div>
-                <img src="{{ asset('images/img2.jpg') }}" style="width:100%">
+                <img src="{{ asset('images/img2.jpg') }}">
                 <div class="text">Caption Two</div>
             </div>
         
             <div class="mySlides fade">
                 <div class="numbertext">3 / 4</div>
-                <img src="{{ asset('images/img2.jpg') }}" style="width:100%">
+                <img src="{{ asset('images/img2.jpg') }}">
                 <div class="text">Caption Three</div>
             </div>
 
             <div class="mySlides fade">
                 <div class="numbertext">4 / 4</div>
-                <img src="{{ asset('images/img2.jpg') }}" style="width:100%">
+                <img src="{{ asset('images/img2.jpg') }}">
                 <div class="text">Caption Four</div>
             </div>
         
@@ -70,7 +70,7 @@
             </div>
         </main>
 
-         
+    <script src="{{ asset('js/home.js') }}"></script>     
     </body>
     @endsection
 </html>
