b9f77093a7661f06822127bb0224ba660ba5970d
jhet <jhetdizon41@gmail.com>
2024-07-13 15:27:26 +0800

    come eat

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,41 +16,19 @@
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">        
     </head>
     <body>     
-        <header class="header">
-            <div class="container">
-                <div class="logo">
-                    <img src="{{ asset('images/logo.png') }}" alt="Meals on Wheels Logo">
-                </div>
-                <div class="user-info">
-                    <div class="user-avatar">
-                        <img src="path/to/user-avatar.png" alt="User Avatar">
-                    </div>
-                    <div class="user-name">Public User</div>
-                    @if (Route::has('login'))
-                        <div class="dropdown">
-                            <button class="dropdown-toggle">▼</button>
-                            <div class="dropdown-menu">
-                                @auth
-                                    <a href="{{ url('/dashboard') }}">Dashboard</a>
-                                @else
-                                    <a href="{{ route('login') }}">Log in</a>
-                                    @if (Route::has('register'))
-                                        <a href="{{ route('register') }}">Register</a>
-                                    @endif
-                                @endauth
-                            </div>
-                        </div>
-                    @endif
-                </div>
-            </div>
-        </header>
+    @section('title')
+    Welcome
+    @endsection
+    
+    @extends('layouts.app')
+    
+    @section('content')
 
         <main>
             <h1>utot</h1>
         </main>
 
-        <footer>
-            ©2024 Meals on Wheels
-        </footer>      
+         
     </body>
+    @endsection
 </html>
