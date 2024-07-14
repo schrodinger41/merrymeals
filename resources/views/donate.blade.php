f62aa98dd90e832a5d0652e8970820a24457e3a8
Kohl <mikaorador@gmail.com>
2024-07-14 09:56:21 +0800

    commit donate

---
+++ resources/views/donate.blade.php
@@ -0,0 +1,10 @@
+<form>
+    <fieldset>
+      <legend>Donation Form:</legend>
+      <label for="fname">First name:</label><br>
+      <input type="text" id="fname" name="fname" value="John"><br>
+      <label for="lname">Last name:</label><br>
+      <input type="text" id="lname" name="lname" value="Doe"><br><br>
+      <input type="submit" value="Submit">
+    </fieldset>
+  </form>
