f1a1f20fbeda81b6537a898472c63e3992accd1b
Kohl <mikaorador@gmail.com~>
2024-07-14 09:22:10 +0800

    commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,69 +1,68 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-   
+
 
 <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Meals on Wheels</title>
+    <title>Meals on Wheels</title>
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 
 
-        <!-- CSS -->
-        <link href="{{ asset('css/home.css') }}" rel="stylesheet">  
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">        
-    </head>
-    <body>     
+    <!-- CSS -->
+    <link href="{{ asset('css/home.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
     @section('title')
-    Welcome
+        Welcome
     @endsection
-    
-    @extends('layouts.app')
-    
-    @section('content')
 
+    @extends('layouts.app')
+
+    @section('content')
         <main>
 
             <!-- Slideshow container -->
             <div class="slideshow-container">
 
-            <!-- Full-width images with number and caption text -->
-            <div class="mySlides fade">
-<<<<<<< HEAD
-                <div class="numbertext">1 / 4</div>
-                <img src="{{ asset('images/img2.jpg') }}">
-                <div class="text">Seniors Test</div>
-=======
-                <img src="{{ asset('images/img1.jpg') }}">
-                <div class="text">Provide <span style="color: var(--secondary-color);">Healthy</span> Meals</div>
->>>>>>> e5eac1bb868e53481d295e20c703fcedea41b687
-            </div>
-        
-            <div class="mySlides fade">
-                <img src="{{ asset('images/img2.jpg') }}">
-                <div class="text">Meals <span style="color: var(--accent-color);">Packed</span> with <span style="color: var(--accent-color);">Care</span></div>
-            </div>
-        
-            <div class="mySlides fade">
-                <img src="{{ asset('images/img3.jpg') }}">
-                <div class="text"><span style="color: #FF7F7F;">Volunteer</span> to <span style="color: #FF7F7F;">Make</span> a <span style="color: #FF7F7F;">Difference</span></div>
-            </div>
+                <!-- Full-width images with number and caption text -->
+                <div class="mySlides fade">
+                    <div class="numbertext">1 / 4</div>
+                    <img src="{{ asset('images/img2.jpg') }}">
+                    <div class="text">Seniors Test</div>
+                    <img src="{{ asset('images/img1.jpg') }}">
+                    <div class="text">Provide <span style="color: var(--secondary-color);">Healthy</span> Meals</div>
+                </div>
 
-            <div class="mySlides fade">
-                <img src="{{ asset('images/img4.jpg') }}">
-                <div class="text">Deliver Meals with <span style="color: var(--primary-color);">Dedication</span></div>
-            </div>
-        
-            <!-- Next and previous buttons -->
+                <div class="mySlides fade">
+                    <img src="{{ asset('images/img2.jpg') }}">
+                    <div class="text">Meals <span style="color: var(--accent-color);">Packed</span> with <span
+                            style="color: var(--accent-color);">Care</span></div>
+                </div>
+
+                <div class="mySlides fade">
+                    <img src="{{ asset('images/img3.jpg') }}">
+                    <div class="text"><span style="color: #FF7F7F;">Volunteer</span> to <span
+                            style="color: #FF7F7F;">Make</span> a <span style="color: #FF7F7F;">Difference</span></div>
+                </div>
+
+                <div class="mySlides fade">
+                    <img src="{{ asset('images/img4.jpg') }}">
+                    <div class="text">Deliver Meals with <span style="color: var(--primary-color);">Dedication</span>
+                    </div>
+                </div>
+
+                <!-- Next and previous buttons -->
                 <a class="prev" onclick="plusSlides(-1)"></a>
                 <a class="next" onclick="plusSlides(1)"></a>
             </div>
             <br>
-            
+
             <!-- The dots/circles -->
             <div class="dot-container">
                 <span class="dot" onclick="currentSlide(1)"></span>
@@ -76,14 +75,18 @@
             <div class="home-content">
                 <div class="column">
                     <h2>Join Us</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quam nulla porttitor massa id neque aliquam vestibulum.
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quam nulla porttitor massa id neque aliquam vestibulum.
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                        et dolore magna aliqua. Quam nulla porttitor massa id neque aliquam vestibulum.
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                        et dolore magna aliqua. Quam nulla porttitor massa id neque aliquam vestibulum.
                     </p>
                 </div>
                 <div class="column">
                     <h2>Our Mission</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quam nulla porttitor massa id neque aliquam vestibulum.
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quam nulla porttitor massa id neque aliquam vestibulum.
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                        et dolore magna aliqua. Quam nulla porttitor massa id neque aliquam vestibulum.
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                        et dolore magna aliqua. Quam nulla porttitor massa id neque aliquam vestibulum.
                     </p>
                 </div>
             </div>
@@ -95,12 +98,11 @@
 
         </main>
 
-    <script src="{{ asset('js/home.js') }}"></script>     
+        <script src="{{ asset('js/home.js') }}"></script>
     </body>
-    @endsection
+@endsection
 
 
 
 
 </html>
-
