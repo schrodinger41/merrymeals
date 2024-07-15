c6f46edd88b9e0718b0b6ae71041036a8fd62e81
bbJerr <jerrakirstenr22@gmail.com>
2024-07-15 22:14:56 +0800

    volunteer home

--- resources/views/Users/Volunteer/volunteerIndex.blade.php
+++ resources/views/Users/Volunteer/volunteerIndex.blade.php
@@ -1,10 +1,33 @@
-@section('title')
-    Welcome
-@endsection
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 
-@extends('Users.Volunteer.layouts.app')
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-@section('content')
+    <title>Volunteer Home - Meals on Wheels</title>
 
-@endsection
+    <!-- CSS -->
+    <link href="{{ asset('css/volunteerHome.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+
+<body>
+    @section('title')
+        Welcome Volunteer
+    @endsection
+
+    @extends('Users.Volunteer.layouts.app')
+
+    @section('content')
+        <div class="background-container">
+            <img src="{{ asset('images/volunteer_home_img.jpg') }}" class="background-image">
+            <div class="content">
+                <h1>Delivering <span>More</span> Than Just Meals</h1>
+                <a href="{{ route('deliver#AllDeliveryForVolunteer') }}" class="button">View Deliveries</a>
+            </div>
+        </div>
+    @endsection
+</body>
+</html>
