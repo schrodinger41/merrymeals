3596cac8e8a489214f5a5f439e739c587d45051f
bbJerr <jerrakirstenr22@gmail.com>
2024-07-17 19:42:27 +0800

    admin update volunteer

--- resources/views/Users/Admin/updateVolunteer.blade.php
+++ resources/views/Users/Admin/updateVolunteer.blade.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <!--  Enable Bootstrap -->
+    <!-- Enable Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
@@ -19,6 +19,7 @@
 </head>
 
 <body>
+
 @section('title')
     Welcome
 @endsection
@@ -27,70 +28,65 @@
 
 @section('content')
 		
-<div id="fh5co-services-section">
+<div id="fh5co-services-section" style="margin-top: 120px; margin-bottom: 30px">
 
-	@if (Session::has('dataInform'))
-		<h4 class="alert alert-warning animate-box text-center" role="alert">
-			{{ Session::get('dataInform') }}
-		</h4>
-	@endif
-	<div class="container">
-		<div class="row display-flex justify-content">
-			<div class="col-md-6 col-sm-6 col-md-offset-3">
-				<div class=" animate-box">
-					<div class="col-md-12 ">
-						<h3>General Information</h3>
-						<form action="{{ route('admin#userUpdated', $userData->id) }}" method="POST">
-							@csrf
-							<label class="userManagement">Name</label><br>
-							<input name="name" class="input-md col-md-12" type="text" value="{{ old('name', $userData->name) }}"/>
+    <div class="row p-3 mt-3 mb-5 container">
+        <div class="col-12">
+            <h3 style="font-weight: var(--weight-700); color: var(--primary-color)"> EDIT VOLUNTEER DETAILS </h3>
+            <hr>
+        </div>
+    </div>
 
-							{{-- <label class="userManagement">Gender</label><br> --}}
-							<input name='gender' class="input-md col-md-12" type="hidden" value="{{ old('gender', $userData->gender) }}"/><br><br>
+    @if (Session::has('dataInform'))
+        <h4 class="alert alert-warning animate-box text-center" role="alert">
+            {{ Session::get('dataInform') }}
+        </h4>
+    @endif
+    <div class="container">
+        <div class="row">
+            <div class="d-flex justify-content-around" style="color: var(--primary-color); font-weight: 600;">
+                <div class="animate-box" style="box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19); width: 45%">
+                    <div class="p-5">
+                        <h3 class="text-center pb-3" style="font-size: var(--h3-font-size); font-weight: 900; color: var(--primary-color)">GENERAL INFORMATION</h3>
+                        <form action="{{ route('admin#userUpdated', $userData->id) }}" method="POST">
+                            @csrf
+                            <label class="userManagement">Name</label><br>
+                            <input name="name" class="input-md col-md-12" type="text" required="required" value="{{ old('name', $userData->name) }}" style="border: 1px var(--secondary-color) solid; padding: 6px"/>
 
-							<label class="userManagement">Age</label><br>
-							<input name="age" class="input-md col-md-12" type="text" value="{{ old('age', $userData->age) }}"/><br><br>
+                            {{-- <label class="userManagement">Gender</label><br> --}}
+                            <input name='gender' class="input-md col-md-12" type="hidden" required="required" value="{{ old('gender', $userData->gender) }}" style="border: 1px var(--secondary-color) solid; padding: 6px"/><br><br>
 
-							<label class="userManagement">Contact</label><br>
-							<input name='phone' class="input-md col-md-12" type="text" value="{{ old('phone', $userData->phone) }}"/><br><br>
+                            <label class="userManagement">Age</label><br>
+                            <input name="age" required="required" class="input-md col-md-12" type="text" value="{{ old('age', $userData->age) }}" style="border: 1px var(--secondary-color) solid; padding: 6px"/><br><br>
 
-							<label class="userManagement">Address</label><br>
-							<input name="address" class="input-md col-md-12" type="text" value="{{ old('address', $userData->address) }}"/><br><br>
+                            <label class="userManagement">Contact</label><br>
+                            <input name='phone' required="required" class="input-md col-md-12" type="text" value="{{ old('phone', $userData->phone) }}" style="border: 1px var(--secondary-color) solid; padding: 6px"/><br><br>
 
-							<label class="userManagement">Email</label><br>
-							<input name='email' class="input-md col-md-12" type="text" value="{{ old('email', $userData->email) }}"/><br><br>
+                            <label class="userManagement">Address</label><br>
+                            <input name="address" required="required" class="input-md col-md-12" type="text" value="{{ old('address', $userData->address) }}" style="border: 1px var(--secondary-color) solid; padding: 6px"/><br><br>
 
-							<div class="text-center"> 
-								<button class="btn-primary">Update</button> &nbsp;
-								<a href="{{ route('admin#allMembers') }}">Cancel</a>
-							</div>
-						</form>
-					</div>
-				</div>
-				<div class=" animate-box">
-					<div class="col-md-12 ">
-						<h3 class="mt-5 pt-5">Volunteer Details</h3>
-						<form action="{{ route('admin#memberUpdated', $userData->id) }}" method="POST">
-							@csrf
-							{{-- <label class="userManagement">Vaccination</label><br> --}}
-							<input name="volunteer_vaccination" class="input-md col-md-12" type="hidden" value="{{ old('volunteer_vaccination', $volunteerData->volunteer_vaccination) }}"/>
+                            <label class="userManagement">Email</label><br>
+                            <input name='email' required="required" class="input-md col-md-12" type="text" value="{{ old('email', $userData->email) }}" style="border: 1px var(--secondary-color) solid; padding: 6px"/><br><br>
 
-							<label class="userManagement">Duration</label><br>
-							<input name="volunteer_duration" class="input-md col-md-12" type="text" value="{{ old('volunteer_duration', $volunteerData->volunteer_duration) }}"/><br><br>
+                            {{-- <label class="userManagement">Vaccination</label><br> --}}
+                            <input name="volunteer_vaccination" class="input-md col-md-12" type="hidden" value="{{ old('volunteer_vaccination', $volunteerData->volunteer_vaccination) }}"/>
 
-							<label class="userManagement">Available</label><br>
-							<input name="volunteer_available" class="input-md col-md-12" type="text" value="{{ old('	volunteer_available', $volunteerData->volunteer_available) }}"/><br><br>
+                            <label class="userManagement">Duration</label><br>
+                            <input name="volunteer_duration" class="input-md col-md-12" type="text" value="{{ old('volunteer_duration', $volunteerData->volunteer_duration) }}" style="border: 1px var(--secondary-color) solid; padding: 6px"/><br><br>
 
-							<div class="text-center"> 
-								<button class="btn-primary">Update</button> &nbsp;
-								<a href="{{ route('admin#allMembers') }}">Cancel</a>
-							</div>
-						</form>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+                            <label class="userManagement">Available</label><br>
+                            <input name="volunteer_available" class="input-md col-md-12" type="text" value="{{ old('volunteer_available', $volunteerData->volunteer_available) }}" style="border: 1px var(--secondary-color) solid; padding: 6px"/><br><br>
+
+                            <div class="text-center pt-2"> 
+                                <button class="btn btn-outline-success">Update</button> &nbsp;
+                                <a href="{{ route('admin#allMembers') }}" class="btn btn-secondary">Cancel</a>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 
 <!-- END What we do -->
